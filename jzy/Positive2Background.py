8c541f783c58f042a0e68c7cae50b32d9505a3d2
Greyhat <1435763997@qq.com>
2022-03-19 03:11:10 +0800

    Update Positive2Background.py

--- jzy/Positive2Background.py
+++ jzy/Positive2Background.py
@@ -3,13 +3,29 @@ Positive cell will be stick randomly on a ROI whit smooth edges
 
 
 
+
+
 '''
 
 import cv2
 import os
-from utils import load_label,mkdirs
+from utils import load_label,mkdirs,coordinate_relative
 
 def is_overlaped(coordinate_pasted,*coordinate_bk):
+    '''
+    判别方法
+        bk_img=zero(h,w)
+        bk_img=1
+        cell_img=1
+
+        if sum(bk_img)=sum(bk_img|cell_img)
+            not overlapped
+        if sum(bk_img)>sum(bk_img|cell_img)
+            overlapped_size=sum(bk_img)-sum(bk_img|cell_img)
+
+
+
+    '''
 
     pass
 
@@ -24,12 +40,19 @@ class Img_Transform():
         '''
         
         paste_num 默认cell 
+
+        流程：  
+            1. 输入 图片,json; 返回img对象和ROI, Cells, FileInfo
+            2. cell_img切出cells(img对象)
+            3. background_img 提供 img对象,和背景原来cell的相对坐标
+            4. 给切出的cells生成随机坐标, 并判断是否与原来cell有交叠,如有则重新生成
+
+
+
+
         '''
         self.cell_img =cv2.cvtColor( cv2.imread(cell_img),cv2.COLOR_BGR2RGB)
         self.bakgground_img = cv2.cvtColor( cv2.imread(bakgground_img),cv2.COLOR_BGR2RGB)
-
-        self.c_Cells=coordinate_trans(cell_label)
-        self.b_Cells=coordinate_trans(bakgground_label)
 
      
 
@@ -62,5 +85,3 @@ if __name__ == '__main__':
 
 
     img_trans=Img_Transform(original_img,bakgground_img,cell_label,bakgground_label,pasted_img)
-
-    pass
