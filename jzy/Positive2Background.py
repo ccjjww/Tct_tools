7fc407c117126e45d01f50fafc18fdb2221a8d0a
Greyhat <1435763997@qq.com>
2022-03-19 03:09:56 +0800

    update

--- jzy/Positive2Background.py
+++ jzy/Positive2Background.py
@@ -5,30 +5,62 @@ Positive cell will be stick randomly on a ROI whit smooth edges
 
 '''
 
+import cv2
+import os
+from utils import load_label,mkdirs
 
 def is_overlaped(coordinate_pasted,*coordinate_bk):
 
     pass
 
 
+
+
+
+
 class Img_Transform():
 
-    def __init__(self, original_img, bakgground_img, original_label):
-        self.bakgground_img = bakgground_img
+    def __init__(self, cell_img, bakgground_img, cell_label,bakgground_label,pasted_img,paste_num=None):
+        '''
+        
+        paste_num 默认cell 
+        '''
+        self.cell_img =cv2.cvtColor( cv2.imread(cell_img),cv2.COLOR_BGR2RGB)
+        self.bakgground_img = cv2.cvtColor( cv2.imread(bakgground_img),cv2.COLOR_BGR2RGB)
 
-        pass
+        self.c_Cells=coordinate_trans(cell_label)
+        self.b_Cells=coordinate_trans(bakgground_label)
+
+     
+
 
     def paste(self):
-        img_cell_pasted = self.bakgground_img
-        label_cell_pasted = self.original_label
+
+
+
+
+
+
+
+
+        
         pass
 
 
 if __name__ == '__main__':
+    
+    os.chdir(r'C:\Users\jzy\Documents\GitHub\Tct_tools\jzy')
 
-    images_dir='./testdir/ROI_Image'
-    labels_dir='./testdir/label'
+    original_img =r'./testdir/ROI_Image/04619.tiffROI_1.Tiff'
+    bakgground_img=r'./testdir/ROI_Image/04891.tiffROI_1.Tiff'
+    
+    cell_label=r'./testdir/label_split_dir/04619_1.json'
+    bakgground_label=r'./testdir/label_split_dir/04891_1.json'
+
+    pasted_img=r'./paste_dir/pasted_img.jpg'
+    mkdirs(pasted_img)
 
 
+    img_trans=Img_Transform(original_img,bakgground_img,cell_label,bakgground_label,pasted_img)
 
     pass
--- jzy/utils.py
+++ jzy/utils.py
@@ -9,12 +9,21 @@ def mkdirs(*dirs_path):
             os.makedirs(dir)
 
 
+def load_label(label_path):
+    with open(label_path, "r", encoding="utf-8") as load_f:
+        data = json.load(load_f)
+        # print(json.dumps(data, indent=4))
+    ROI = data["ROI"]
+    Cells = data["CellRect"]
+    FileInfo = data["FileInfo"]
+    return ROI, Cells, FileInfo
+
+
 def label_split(label_dir, Labels_split_dir):
     labels_list = os.listdir(label_dir)
 
     for label in labels_list:
         label_path = os.path.join(label_dir, label)
-        print(label_path)
 
         with open(label_path, 'r',encoding='utf-8') as load_f:
             data = json.load(load_f)
@@ -31,7 +40,6 @@ def label_split(label_dir, Labels_split_dir):
         for RoiID in RoiIDs:
             label_split_name = os.path.join(Labels_split_dir, str(file_name +
                                                                   '_' + str(RoiID) + '.json'))
-            print(label_split_name)
             ROI = [roi for roi in ROIs if roi['roiID'] == RoiID]
             cells = [cell for cell in Cells if cell['RoiID'] == RoiID]
             label_split = {
@@ -43,9 +51,66 @@ def label_split(label_dir, Labels_split_dir):
                 f.write(label_split)
 
 
-if __name__ == '__main__':
-    label_dir = './testdir/label'
-    label_split_dir = './testdir/label_split_dir'
+def coordinate_relative(ROI, Cells):
+    
+    roi_x = ROI['x']
+    roi_y = ROI['y']
+    for cell in Cells:
+        # TODO：此处是在原处更新，原始值copy之后的新对象也更新，可能引起BUG
+        cell['x'] = int(cell['x'] - roi_x)
+        cell['y'] = int(cell['y'] - roi_y)
+    return Cells
 
+
+
+def cells_cut(img, cells_list, ifsave=False):
+    '''
+    读取img 和label
+    '''
+    
+    cells_cut={}
+    for cell in cells_list:
+        cell_x = int(cell['x'] )
+        cell_y = int(cell['y'] )
+        cell_w = int(cell['w'])
+        cell_h = int(cell['h'])
+
+
+        cell_img=img[cell_y:cell_y+cell_h,cell_x:cell_x+cell_w]
+        cells_cut[cell['ID']]=cell_img
+
+        if ifsave:
+            cell_img_name = os.path.join('./cells', str(cell['ID']) + '.jpg')
+            mkdirs(cell_img_name)
+            cv2.imwrite(cell_img_name, cell_img)
+    
+    return cells_cut
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    os.chdir(r'C:\Users\jzy\Documents\GitHub\Tct_tools\jzy')
+    label_dir = r'./testdir/label'
+    label_split_dir = r'./testdir/label_split_dir'
+
+    # 1 按照ROI拆分json
     mkdirs(label_split_dir)
     label_split(label_dir, label_split_dir)
+
+    # 2 计算相对位置
+    img_path =r'./testdir/ROI_Image/04619.tiffROI_1.Tiff'
+    labe_pathl=r'./testdir/label_split_dir/04619_1.json'
+
+    img=cv2.cvtColor(cv2.imread(img_path),cv2.COLOR_BGR2RGB)
+    ROI, Cells, _=load_label(labe_pathl)
+    Cells_coor_relative=coordinate_relative(ROI[0], Cells)
+
+    # 3 切出cell 
+
+    cells=cells_cut(img,Cells_coor_relative)
+
+    
